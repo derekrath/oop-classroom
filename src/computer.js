6ac417c62e0f7c3378b5caaa17174ea1116fcaa1
Derek Rath <lompoc.prosolutions@gmail.com>
2021-09-16 15:29:21 -0700

    Build computer draft

--- src/computer.js
+++ src/computer.js
@@ -1,5 +1,14 @@
 class Computer {
-  
+	constructor(name, operatingSystem, processor, memory, graphics)
+	this.name = name
+	this.operatingSystem = operatingSystem
+	this.processor = processor
+	this.memory = memory
+	this.graphics = graphics
 }
+// power(on =false) {
+//     this.on=on;
+//     return on;
+// }
+// module.exports = Computer
 
-module.exports = Computer
